3af0212fb624d43762063754779d7a1742b11387
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2024-09-13 21:27:08 +0530

    Create admin and Token verifier middlewares

---
+++ MultiUserSystem/src/middleware/admin.auth.js
@@ -0,0 +1,9 @@
+const adminChecker = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ msg: 'Access denied' });
+    }
+    next();
+  };
+
+export default adminChecker;
+  
